9070059d546f61a4a951e49030171793c1471a91
Alex Sfamurri <alex.sfamurri.504@gmail.com>
2020-09-22 19:50:05 -0500

    crash course script

---
+++ Rotation/Assets/CubeController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey("left")) {
+            this.transform.localRotation *= Quaternion.Euler ( new Vector3(0, 1, 0));
+        }
+        if(Input.GetKey("right")) {
+            this.transform.localRotation *= Quaternion.Euler ( new Vector3(0, -1, 0));
+        }
+        if(Input.GetKey("space")) {
+            this.transform.localRotation = Quaternion.identity;
+        }
+    }
+}
